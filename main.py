7d7326efdb5fd811a5f225d5b56ae107eec2bcd4
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2025-03-25 15:27:16 +0300

    minion game

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def minion_game(string):
-    pass
+import string as st
+def minion_game(input_word):
+    input_word = [letter.capitalize() for letter in input_word]
+    vowels = ["A", "E", "I", "O", "U"]
+    consonants = list(letter for letter in st.ascii_uppercase if letter not in vowels )
+    # print(input_word)
+    kevins_vowel_words = [word for word in input_word if word not in consonants]
+    kevins_consonant_words = [word for word in input_word if word in consonants]
+
+    print(kevins_consonant_words)
 
 if __name__ == '__main__':
     s = input()
