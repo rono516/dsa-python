68a5457bca135a5c816f663964b460fed050c977
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2025-04-01 12:11:32 +0300

    changes

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import string as st
 def minion_game(input_word):
-    input_word = [letter.capitalize() for letter in input_word]
+    input_word = input_word.upper()
+    # print(input_word)
+    input_word_letters = [letter.capitalize() for letter in input_word]
     vowels = ["A", "E", "I", "O", "U"]
     consonants = list(letter for letter in st.ascii_uppercase if letter not in vowels )
     # print(input_word)
-    kevins_vowel_words = [word for word in input_word if word not in consonants]
-    kevins_consonant_words = [word for word in input_word if word in consonants]
+    kevins_vowel_words = list(set(word for word in input_word_letters if word not in consonants))
+    stuarts_consonant_words = list(set(word for word in input_word_letters if word in consonants))
 
-    print(kevins_consonant_words)
+    kevins_vowel_words =kevins_vowel_words + [vowel+consonant+vowel+consonant+vowel for vowel in kevins_vowel_words for consonant in stuarts_consonant_words if input_word.find(vowel+consonant) != -1  ]
+    print(kevins_vowel_words)
 
 if __name__ == '__main__':
     s = input()
