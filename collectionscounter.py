e2e8a6a443180a895933d3050f00527052438ca2
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2025-04-06 18:38:31 +0300

    collections counter resolved

--- collectionscounter.py
+++ collectionscounter.py
@@ -1,24 +1,25 @@
 from collections import Counter
 if __name__ == "__main__":
-    number_of_shoes = int(input("Number of shoes "))
-    all_shoe_sizes = input("All shoe sizes ").split(" ")
-    number_of_customers = int(input("Number of customers "))
+    number_of_shoes = int(input())
+    all_shoe_sizes = input().split(" ")
+    number_of_customers = int(input())
     money_earned = 0
     order_size_and_price = []
     max_count_of_sizes_ordered = 2
 
-    for i in range(number_of_customers):
+    for _ in range(number_of_customers):
         order_size, order_price = input().split(" ")
         order_size_and_price.append([order_size,order_price])
 
     sizes_ordered = []
 
-    for order in order_size_and_price:
-        count = Counter(sizes_ordered)
+    count = Counter(all_shoe_sizes)
 
-        if count[order[0]] < 2:
+    for order in order_size_and_price:
+        if count[order[0]] > 0:
             if order[0] in all_shoe_sizes:
                 sizes_ordered.append(order[0])
                 money_earned += int(order[1])
+                count[order[0]] -=1
     print(money_earned)
 
