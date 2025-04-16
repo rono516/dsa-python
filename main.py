0f58d6b57314ac09fb635514641364a58eef3aa4
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2025-04-16 15:25:37 +0300

    angles

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-from math import sqrt
-if __name__ == "__main__":
-    a = 10
-    b = 10
-    hypotenuse = a*a + b*b
-    print(sqrt(hypotenuse))
+import math
 
-    
+class Complex(object):
+    def __init__(self, real, imaginary):
+        pass
+
+        
+    def __add__(self, no):
+        pass
+        
+    def __sub__(self, no):
+        pass
+    def __mul__(self, no):
+        pass
+    def __truediv__(self, no):
+        pass
+
+    def mod(self):
+        pass
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
