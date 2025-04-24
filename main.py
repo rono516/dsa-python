1edd101f0d6874adc06ba3b21ba93f1f58e10812
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2025-04-24 15:51:13 +0300

    leetcode problem

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from math import sqrt
+# from math import sqrt
+# if __name__ == "__main__":
+#     a = 10
+#     b = 10
+#     hypotenuse = a*a + b*b
+#     print(sqrt(hypotenuse))
+
+
+# leetcode problem - https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
 if __name__ == "__main__":
-    a = 10
-    b = 10
-    hypotenuse = a*a + b*b
-    print(sqrt(hypotenuse))
+    nums =[4,5,6,7,0,1,2]
+    target = 0
+    location = None
+
+    # if target in nums:
+    #     print("There")
+    # else :
+    #     print("Not there")
+
+    for i in range(len(nums)):
+        if nums[i] == target:
+            location = i
+
+    if location:
+        print(location)
+    else:
+        print(-1)
+
 
     
